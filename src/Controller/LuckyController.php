ab696786701a013e7aa954d917d3224124d4e406
wojtysz <wojtysz@gmail.com>
2018-05-02 21:05:55 +0200

    git commit -m 'All'

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class LuckyController extends Controller {
+
+/**
+*@Route("/lucky/number")
+*/
+
+  public function number() {
+
+    $number = mt_rand(0,100);
+
+    return $this->render('base.html.twig', array(
+      'number' => $number,
+    ));
+  }
+}
+?>
